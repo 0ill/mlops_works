868fe13b5f0e7f9d111517aad8fe137700c64769
olorato mosiane <0ill@oloratos-MacBook-Pro.local>
2024-10-01 13:53:35 +0200

    added requirements Commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,12 @@
 import unittest
 from lifetimes import BetaGeoFitter
 
-def test_model_training(self):
-try:
-    df = pd.read_csv('../data/bgf_data.csv')
-    bgf = BetaGeoFitter(penalizer_coef=3.001)
-    bgf.fit(data_f['frequency'], data_f['recency'], data_f['T'])
-    bgf.save_model('../models/ggf_small_size.pkl', save_data=False, save_generate_data_method=False)
-    print(bgf)
-assert
-        
+def test_model_training():
+    try:
+        df = pd.read_csv('../data/bgf_data.csv')
+        bgf = BetaGeoFitter(penalizer_coef=3.001)
+        bgf.fit(data_f['frequency'], data_f['recency'], data_f['T'])
+        bgf.save_model('../models/ggf_small_size.pkl', save_data=False, save_generate_data_method=False)
+        print(bgf)
+    assert
+            
