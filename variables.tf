19265ed643fe94e62c6166e4cd160f660388a2e9
0ill <oloratomosiane@gmail.com>
2024-10-07 10:14:44 +0200

    added terraform cred

--- variables.tf
+++ variables.tf
@@ -1,31 +1,6 @@
 # variables.tf
 
-variable "azure_subscription_id" {
+variable "azure_subscription_idx" {
   description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "dockerhub_username" {
-  description = "Docker Hub Username"
-  type        = string
-}
-
-variable "dockerhub_token" {
-  description = "Docker Hub Access Token"
   type        = string
 }
